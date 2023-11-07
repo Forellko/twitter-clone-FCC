a03792ff782c9c23ddb5bff5741052698c5fe97e
forellko <dragonforce97q@gmail.com>
2023-11-07 14:51:09 +0300

    fix core layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="bg-red-400 w-full h-full flex justify-center items-center">
-      <div className="max-w-screen-lg w-full h-full flex relative">1</div>
-      <section></section>
-      <main></main>
-      <section></section>
+      <div className="max-w-screen-lg w-full h-full flex relative">
+        <section></section>
+        <main></main>
+        <section></section>
+      </div>
     </div>
   );
 }
