b7e6ebffe5a16f0434acfd4a31420e56487377eb
forellko <dragonforce97q@gmail.com>
2023-11-07 14:50:35 +0300

    Add core layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="bg-red-400 w-full h-full flex justify-center items-center">
+      <div className="max-w-screen-lg w-full h-full flex relative">1</div>
+      <section></section>
+      <main></main>
+      <section></section>
+    </div>
+  );
 }
