b47d6475f211c5d952bc185188983c5bbc5417a7
forellko <dragonforce97q@gmail.com>
2023-11-09 20:10:32 +0300

    Change nav. Just for 1 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { BiHomeCircle, BiUser } from 'react-icons/bi';
 import { HiOutlineHashtag } from 'react-icons/hi';
-import { BsBell, BsBookmark } from 'react-icons/bs';
+import { BsBell, BsBookmark, BsTwitter } from 'react-icons/bs';
 import { HiEnvelope } from 'react-icons/hi2';
 import Link from 'next/link';
 
@@ -35,11 +35,14 @@ export default function Home() {
   return (
     <div className="bg-red-400 w-full h-full flex justify-center items-center">
       <div className="max-w-screen-lg w-full h-full flex relative">
-        <section className="fixed w-72 flex flex-col h-screen">
+        <section className="fixed w-72 flex flex-col h-screen space-y-4 my-4">
+          <Link href={'/'} className="p-2 text-2xl">
+            <BsTwitter />
+          </Link>
           {NAVIGATION_ITEMS.map((item) => (
             <Link
               key={item.title}
-              className="bg-white/50 flex items-center justify-center rounded-3xl p-4 space-x-2"
+              className="hover:bg-white/10 transition duration-200  flex items-center justify-start w-fit rounded-3xl p-2 space-x-2 text-2xl"
               href={`/${item.title.toLowerCase()}`}
             >
               <div>
